672236de831f91c014c70a27bc17ecb3f87d7630
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-07-15 20:45:29 +0530

    refactor UpiMethod component to manage payment options and handles dynamically

--- src/components/UpiMethod.jsx
+++ src/components/UpiMethod.jsx
@@ -1,13 +1,56 @@
 import { Button, DropdownMenu, TextField } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 
-import React from "react";
+const paymentOptions = [
+  {
+    name: "Google Pay",
+    image:
+      "https://www.svgrepo.com/show/303357/google-pay-primary-logo-logo.svg",
+    upiHandles: ["@oksbi", "@okaxis", "@okhdfcbank", "@okicici", "@okyesbank"],
+    class: "w-8 h-8",
+  },
+  {
+    name: "PhonePe",
+    image:
+      "https://cdn.iconscout.com/icon/free/png-512/free-phonepe-logo-icon-download-in-svg-png-gif-file-formats--payment-app-application-indian-companies-pack-logos-icons-2249157.png?f=webp&w=512",
+    upiHandles: ["@ybl", "@ibl"],
+    class: "w-16 h-auto",
+  },
+  {
+    name: "Paytm",
+    image:
+      "https://cdn.iconscout.com/icon/free/png-512/free-paytm-logo-icon-download-in-svg-png-gif-file-formats--online-payment-brand-logos-pack-icons-226448.png?f=webp&w=512",
+    upiHandles: ["@paytm"],
+    class: "w-8 h-8",
+  },
+];
 
 const UpiMethod = () => {
+  const [option, setOption] = useState({
+    name: "Google Pay",
+    image:
+      "https://www.svgrepo.com/show/303357/google-pay-primary-logo-logo.svg",
+    upiHandles: ["@oksbi", "@okaxis", "@okhdfcbank", "@okicici", "@okyesbank"],
+    class: "w-8 h-8",
+  });
+  const [options, setOptions] = useState([]);
+  const [handle, setHandle] = useState(option?.upiHandles[0]);
+  const [handles, setHandles] = useState([]);
+
+  useEffect(() => {
+    setOptions(paymentOptions.filter((app) => app.name !== option.name));
+    setHandles(option.upiHandles.filter((each) => each !== handle));
+  }, [option, handle]);
+
+  useEffect(() => {
+    setHandle(option?.upiHandles[0]);
+  }, [option]);
+
   return (
-    <div className="w-full h-fit">
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
+    <div className="w-full h-full">
+      <div className="flex flex-col gap-4 w-full">
+        <div className="flex flex-col gap-2 w-full">
           <div className="text-xs">
             <p>Select your UPI app</p>
           </div>
@@ -16,31 +59,29 @@ const UpiMethod = () => {
               <Button color="gray" variant="outline" className="w-full">
                 <div className="flex flex-row justify-between items-center w-full text-start">
                   <img
-                    alt="upi"
-                    class="object-fill h-8 w-8"
-                    src="https://www.svgrepo.com/show/303357/google-pay-primary-logo-logo.svg"
+                    alt={option.name}
+                    class={`object-fill ${option.class}`}
+                    src={option.image}
                   />
                   <IoIosArrowDown />
                 </div>
               </Button>
             </DropdownMenu.Trigger>
 
-            <DropdownMenu.Content className="w-full flex flex-col items-center">
-              <DropdownMenu.Item className="flex flex-row justify-between w-fit m-auto focus:bg-gray-100 hover:bg-gray-100 outline-none">
-                <img
-                  alt="upi"
-                  className="object-fill w-16 h-auto"
-                  src="https://cdn.iconscout.com/icon/free/png-512/free-phonepe-logo-icon-download-in-svg-png-gif-file-formats--payment-app-application-indian-companies-pack-logos-icons-2249157.png?f=webp&w=512"
-                />
-              </DropdownMenu.Item>
-
-              <DropdownMenu.Item className="w-full data-[highlighted]:bg-gray-200 data-[highlighted]:text-black outline-none">
-                <img
-                  alt="upi"
-                  className="object-fill w-8 h-8 m-auto"
-                  src="https://cdn.iconscout.com/icon/free/png-512/free-paytm-logo-icon-download-in-svg-png-gif-file-formats--online-payment-brand-logos-pack-icons-226448.png?f=webp&w=512"
-                />
-              </DropdownMenu.Item>
+            <DropdownMenu.Content className="w-full flex flex-col gap-2 items-center">
+              {options.map((method) => (
+                <DropdownMenu.Item
+                  onClick={() => setOption(method)}
+                  key={method.name}
+                  className="flex w-auto flex-row justify-between m-auto focus:bg-gray-100 hover:bg-gray-100 outline-none"
+                >
+                  <img
+                    alt={method.name}
+                    className={`object-fill ${method.class}`}
+                    src={method.image}
+                  />
+                </DropdownMenu.Item>
+              ))}
             </DropdownMenu.Content>
           </DropdownMenu.Root>
         </div>
@@ -55,11 +96,21 @@ const UpiMethod = () => {
                 <DropdownMenu.Trigger className="w-fit">
                   <Button color="gray" variant="outline" className="w-fit">
                     <div className="flex flex-row gap-1 items-center w-fit text-start">
-                      <span>@okaysbi</span>
-                      <IoIosArrowDown />
+                      <span>{handle}</span>
+                      {handles.length > 1 && <IoIosArrowDown />}
                     </div>
                   </Button>
                 </DropdownMenu.Trigger>
+
+                {handles.length > 1 && (
+                  <DropdownMenu.Content>
+                    {handles.map((handle) => (
+                      <DropdownMenu.Item onClick={() => setHandle(handle)}>
+                        <span>{handle}</span>
+                      </DropdownMenu.Item>
+                    ))}
+                  </DropdownMenu.Content>
+                )}
               </DropdownMenu.Root>
             </div>
           </div>
@@ -67,7 +118,9 @@ const UpiMethod = () => {
 
         {/* verify button */}
         <div className="w-full">
-          <Button className="w-full" color="green" variant="solid">Verify & Pay</Button>
+          <Button className="w-full" color="green" variant="solid">
+            Verify & Pay
+          </Button>
         </div>
       </div>
     </div>
