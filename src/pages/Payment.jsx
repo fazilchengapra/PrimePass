e77647644afccd368448e29841ac5c737cd2c0de
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-09-25 14:36:19 +0530

    fix: include user email in payment processing for better tracking

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -26,6 +26,7 @@ const Payment = () => {
 
   const movie = useSelector((state) => state.movie.selectedMovie);
   const theater = useSelector((state) => state?.theater?.theater);
+  const mail = useSelector((state) => state?.user.email);
 
   const [bookingDetails, setBookingDetails] = useState({});
   const [loading, setLoading] = useState(false); // preparing Razorpay
@@ -55,6 +56,7 @@ const Payment = () => {
               razorpay_order_id: response.razorpay_order_id,
               razorpay_payment_id: response.razorpay_payment_id,
               razorpay_signature: response.razorpay_signature,
+              mail
             });
             toast.success("Payment Success!");
             navigate(`/order/${result.data.data.id}`);
@@ -71,6 +73,7 @@ const Payment = () => {
 
       const rzp = new window.Razorpay(options);
       rzp.open();
+      
     } catch (err) {
       console.error(err);
       toast.error("Payment Failed!");
